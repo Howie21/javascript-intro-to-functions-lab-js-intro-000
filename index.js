1f205f1a7e04840d818a94f709b489631a6de108
Wesley B. Howard <bryce9643@gmail.com>
2021-02-05 23:22:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string) {
+  if string.lowercase() {
+    return ("I can't hear you!")
+  }
+  if string.uppercase() {
+    return ("YES INDEED!")
+  }
+  if string("I love you, Grandma.") {
+    return ("I love you, too.")
+  }
+}
